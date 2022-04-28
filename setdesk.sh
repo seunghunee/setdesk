c3b96d3b59d7718b7f8ed6dce3be151d23a5af0c
Shin Seunghun <seunghunee@gmail.com>
2022-04-28 11:57:06 +0900

    Fix: Ubuntu 22.04에서 실행가능 하도록 조정

--- setdesk.sh
+++ setdesk.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ $EUID != 0 ]; then
-    echo Run "$0" with sudo: sudo "$0"
-    exit 1
-fi
-
 if ! type ansible &> /dev/null; then
-    apt install ansible -y
+    sudo apt install ansible -y
 fi
 
-sudo -u $(logname) ansible-playbook $(dirname $0)/setdesk.yml
+ansible-playbook $(dirname $0)/setdesk.yml --ask-become-pass
