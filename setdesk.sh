f32018ba01178356615f9ee1bb210b07c8458c4e
Shin Seunghun <seunghunee@gmail.com>
2024-10-14 14:59:29 +0900

    Fix Ansible warning by adding --connection=local option

--- setdesk.sh
+++ setdesk.sh
@@ -4,4 +4,4 @@ if ! type ansible &> /dev/null; then
     sudo apt install ansible -y
 fi
 
-ansible-playbook $(dirname $0)/setdesk.yml --ask-become-pass
+ansible-playbook -i localhost, --connection=local $(dirname $0)/setdesk.yml --ask-become-pass
